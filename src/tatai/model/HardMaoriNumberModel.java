debd809486a5b5312b6a6e07912a2f0d71885635
Skippy <samuelzheng11@gmail.com>
2017-09-08 00:27:13 +1200

    added the HardMaoriNumberModel class which calls the constructor in the number model class to populate the list of maori words used for the hard difficulty of the game, it does this by passing in two arguments representing the max and min values making up the range of the hard difficulty, very similar to the easy mode model

---
+++ src/tatai/model/HardMaoriNumberModel.java
@@ -0,0 +1,20 @@
+package tatai.model;
+
+public class HardMaoriNumberModel extends NumberModel {
+
+	// private final variables that are used to store the maximum and minimum values
+	// the maori numbers can be for the hard mode of the game. (in the hard mode the
+	// range is 1 - 99)
+	// these in addition also make it easier to maintain as it reduces magic numbers
+	// scattered throughout the class
+	private static final int _MAX_VALUE = 99;
+	private static final int _MIN_VALUE = 1;
+
+	// public constructor used for creating an hard mode number model (1 - 99 in
+	// maori)
+	// that is used for in the hard mode of the tatai game
+	public HardMaoriNumberModel() {
+		super(_MAX_VALUE, _MIN_VALUE);
+		System.out.println(_currentNumber);
+	}
+}
