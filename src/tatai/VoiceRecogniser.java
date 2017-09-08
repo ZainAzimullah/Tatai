027330fa8084755afec2f2ec49da16072ab72388
Zain Azimullah (Linux) <zain.zain51@gmail.com>
2017-09-08 23:00:54 +1200

    Implemented VoiceRecogniser and SpeechNotFoundException

--- src/tatai/VoiceRecogniser.java
+++ src/tatai/VoiceRecogniser.java
@@ -1,5 +1,41 @@
 package tatai;
 
+import java.util.List;
+
+import tatai.bashTools.BashCommand;
+import tatai.exceptions.SpeechNotFoundException;
+
 public class VoiceRecogniser {
 	
+	public String getSpeech(String filename) throws SpeechNotFoundException {
+		BashCommand bash = new BashCommand();
+		
+		bash.runCommand("HVite -H HMMs/hmm15/macros -H "
+				+ "HMMs/hmm15/hmmdefs -C user/configLR  -w "
+				+ "user/wordNetworkNum -o SWT -l '*' -i recout.mlf "
+				+ "-p 0.0 -s 5.0  user/dictionaryD user/tiedList " + filename);
+		bash.runCommand("rm -f " + filename);
+		
+		List<String> output = bash.getLog();
+		
+		String speech = null;
+		boolean readyToGet = false;
+		
+		for (String line: output) {
+			if (readyToGet) {
+				speech = line;
+				break;
+			}
+			
+			if (line.equals("sil")) {
+				readyToGet = true;
+			}
+		}
+		
+		if (speech == null) {
+			throw new SpeechNotFoundException("No voice observed");
+		}
+		
+		return speech;
+	}
 }
--- src/tatai/bashTools/TestHTK.java
+++ src/tatai/bashTools/TestHTK.java
@@ -6,11 +6,14 @@ public class TestHTK {
 	public static void main(String[] args) {
 		BashCommand bash = new BashCommand();
 		bash.runCommand("arecord -d 2 -r 22050 -c 1 -i -t wav -f s16_LE foo.wav");
+		
+		bash.runCommand("ffplay -autoexit -nodisp foo.wav &> /dev/null");
+		
 		bash.runCommand("HVite -H HMMs/hmm15/macros -H "
 				+ "HMMs/hmm15/hmmdefs -C user/configLR  -w "
 				+ "user/wordNetworkNum -o SWT -l '*' -i recout.mlf "
 				+ "-p 0.0 -s 5.0  user/dictionaryD user/tiedList foo.wav");
-		bash.runCommand("ffplay -autoexit -nodisp foo.wav &> /dev/null");
+		
 		bash.runCommand("rm foo.wav");
 		bash.runCommand("cat recout.mlf");
 		
