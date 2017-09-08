8e01325d9847052ef07662a3914e745d9ff58f18
Skippy <samuelzheng11@gmail.com>
2017-09-08 15:28:55 +1200

    fixed the bug in the abstract Maori number model where it was only printing one number, fully commented the fix

--- src/tatai/model/MaoriNumberModel.java
+++ src/tatai/model/MaoriNumberModel.java
@@ -1,36 +1,42 @@
 package tatai.model;
 
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Random;
 
 public abstract class MaoriNumberModel {
 
+	//variables used to keep track of what position we are in on the Maori number model
 	protected MaoriNumber _currentNumber;
-	protected List<MaoriNumber> _numbers = new LinkedList<MaoriNumber>();
-
+	private int _currentNumberPosition;
+	//field used for storing the MaoriNumberModel
+	protected MaoriNumber[] _numbers = new MaoriNumber[10];
+	
+	//abstract constructor used by children to create an array of MaoriNumbers in a range
 	protected MaoriNumberModel(int maxValue, int minValue) {
 		Random rand = new Random();
 
 		for (int index = 0; index < 10; index++) {
 			// creating a number between the range of minValue - maxValue and using it to
 			// generate
-			// a Maori number that is then stored in a list to set up the stages for the
+			// a Maori number that is then stored in an array to set up the stages for the
 			// easy game mode
-			_numbers.add(new MaoriNumber(rand.nextInt(maxValue) + minValue));
+			_numbers[index] =(new MaoriNumber(rand.nextInt(maxValue) + minValue));
 		}
 
 		// setting the very first number in the sequence of number to the variable
 		// assigned for keeping
 		// track of the current number being used in the game.
-		_currentNumber = _numbers.iterator().next();
+		_currentNumber = _numbers[0];
+		_currentNumberPosition = 0;
 
 	}
 
+	//used to get to the next item in the array storing the randomly generated Maori number
 	public void advance() {
-		_currentNumber = _numbers.iterator().next();
+		_currentNumberPosition++;
+		_currentNumber = _numbers[_currentNumberPosition];
 	}
-
+	
+	//getter for returning the current MaoriNumber Object we are pointing at
 	public MaoriNumber getCurrentMaoriNumber() {
 		return _currentNumber;
 	}
