312f1f849605c704ea3446437d2329acb0600edc
Zain Azimullah (Linux) <zain.zain51@gmail.com>
2017-09-08 16:37:22 +1200

    Added bashTools package

--- src/tatai/bashTools/BashCommand.java
+++ src/tatai/bashTools/BashCommand.java
@@ -1,4 +1,4 @@
-package tatai;
+package tatai.bashTools;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
---
+++ src/tatai/bashTools/BashStandardOutput.java
@@ -0,0 +1,5 @@
+package tatai.bashTools;
+
+public class BashStandardOutput extends BashCommand {
+
+}
