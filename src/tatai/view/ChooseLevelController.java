b36804c4dbb862a6cfa729e2122693773939d04f
Skippy <samuelzheng11@gmail.com>
2017-09-08 17:28:13 +1200

    linked up the hard and easy methods and buttons, return to main menu button still has NOT been linked

--- src/tatai/view/ChooseLevelController.java
+++ src/tatai/view/ChooseLevelController.java
@@ -2,6 +2,7 @@ package tatai.view;
 
 
 import javafx.fxml.FXML;
+import javafx.stage.Stage;
 import tatai.Game;
 import tatai.Level;
 
@@ -10,18 +11,30 @@ import tatai.Level;
  */
 public class ChooseLevelController extends SceneController {
 	
+	
+	
 	@FXML
 	private void chooseEasy() {
+		//TODO: find a way to create an easy model
 		Game.getInstance().createList(Level.EASY);
+		//System.out.println("Easy button working");
 	}
 	
 	@FXML
 	private void chooseHard() {
+		//TODO: find a way to create a hard level model
 		Game.getInstance().createList(Level.HARD);
+		//System.out.println("Hard button working");
 	}
 	
 	@FXML
 	private void initialize() {
 		
+	}
+	
+	@FXML
+	private void returnToMainMenu() {
+		//TODO: implement way to get back to main menu
+		Stage stage = Game.getInstance().getStage();
 	}
 }
