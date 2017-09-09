cae9fca293e9bd3f086605a8044e70e9c570957c
Zain Azimullah (Linux) <zain.zain51@gmail.com>
2017-09-09 15:46:19 +1200

    Linked Record to its controller

--- src/tatai/view/RecordController.java
+++ src/tatai/view/RecordController.java
@@ -1,5 +1,21 @@
 package tatai.view;
 
-public class RecordController extends SceneController {
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
+public class RecordController extends SceneController {
+	
+	@FXML
+	private Label _label;
+	
+	@FXML
+	private Button _button;
+	
+	@FXML
+	private void record() {
+		_button.setText("RECORDING");
+		_button.setDisable(true);
+		_label.setText("Recording");
+	}
 }
