cd12bd2185c602b8c68e667928730b32ddece4dd
Zain Azimullah (Linux) <zain.zain51@gmail.com>
2017-09-09 21:30:49 +1200

    Fin rec scene now hooked with controller. Need to implement return to main menu button.

--- src/tatai/view/FinishedRecordingController.java
+++ src/tatai/view/FinishedRecordingController.java
@@ -1,5 +1,46 @@
 package tatai.view;
 
-public class FinishedRecordingController extends SceneController {
+import java.io.File;
 
+import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import tatai.Game;
+
+public class FinishedRecordingController extends SceneController {
+	
+	@FXML
+	private Label _message;
+	
+	@FXML
+	private Button _play;
+	
+	@FXML
+	private void playback() {
+		_message.setText("Playing back recording...");
+		_message.setAlignment(Pos.CENTER);
+		_play.setDisable(true);
+		
+		Media media = new Media(new File("foo.wav").toURI().toString());
+		MediaPlayer player = new MediaPlayer(media);
+		
+		player.play();
+		player.setOnEndOfMedia(() -> {
+			Game.getInstance().finishedRecording();
+		});
+		
+	}
+	
+	@FXML
+	private void submit() {
+		Game.getInstance().showResult();
+	}
+	
+	@FXML
+	private void redo() {
+		Game.getInstance().record();
+	}
 }
