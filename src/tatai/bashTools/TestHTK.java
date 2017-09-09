26d19eef7cf4d6b07044494f0a6f8f8408b8f9b2
Zain Azimullah (Linux) <zain.zain51@gmail.com>
2017-09-09 15:14:49 +1200

    Adjust TestHTK to use background thread

--- src/tatai/bashTools/TestHTK.java
+++ src/tatai/bashTools/TestHTK.java
@@ -1,25 +1,58 @@
 package tatai.bashTools;
 
+import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.List;
 
+import javafx.application.Application;
+import javafx.concurrent.Task;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.stage.Stage;
 import tatai.exceptions.SpeechNotFoundException;
 
-public class TestHTK {
+public class TestHTK extends Application {
 	public static void main(String[] args) {
+		launch(args);
+
+	}
+	
+	@Override
+	public void start(Stage stage) {
 		BashCommand bash = new BashCommand();
-		bash.runCommand("arecord -d 2 -r 22050 -c 1 -i -t wav -f s16_LE foo.wav");
+		bash.runCommand("arecord -d 2 -r 22050 -c 1 -i -t wav -f s16_LE foo.wav");		
 		
-		bash.runCommand("ffplay -autoexit -nodisp foo.wav &> /dev/null");
+		new Thread(new Background()).start();
+	}
+	
+	private class Background extends Task<Void> {
+		@Override
+		protected Void call() {
+			Media media = new Media((new File(System.getProperty("user.dir") + "/foo.wav")).toURI().toString());
+			MediaPlayer player = new MediaPlayer(media);
+			player.play();
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			return null;
+		}
 		
-		VoiceRecogniser htk = new VoiceRecogniser();
-		
-		try {
-			System.out.println(htk.getSpeech("foo.wav"));
-		} catch (SpeechNotFoundException e) {
-			System.out.println("You didn't say anything recognisable");
-		} catch (FileNotFoundException e) {
-			System.out.println("Could not find file");
+		@Override
+		protected void succeeded() {
+			VoiceRecogniser htk = new VoiceRecogniser();
+			
+			try {
+				System.out.println(htk.getSpeech("foo.wav"));
+			} catch (SpeechNotFoundException e) {
+				System.out.println("You didn't say anything recognisable");
+			} catch (FileNotFoundException e) {
+				System.out.println("Could not find file");
+			}
+			
+			System.exit(0);
 		}
 	}
 }
