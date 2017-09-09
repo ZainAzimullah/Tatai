9df4cf45b447d9a3054ab32c027d1be28b2a647e
Zain Azimullah (Linux) <zain.zain51@gmail.com>
2017-09-09 16:48:19 +1200

    Recording scene now works and is multithreaded properly.  Still need to implement a timer.

--- src/tatai/view/RecordController.java
+++ src/tatai/view/RecordController.java
@@ -1,8 +1,16 @@
 package tatai.view;
 
+import java.io.File;
+
+import javafx.application.Platform;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import tatai.Game;
+import tatai.bashTools.BashCommand;
 
 public class RecordController extends SceneController {
 	
@@ -17,5 +25,23 @@ public class RecordController extends SceneController {
 		_button.setText("RECORDING");
 		_button.setDisable(true);
 		_label.setText("Recording");
+		new Thread(new Background()).start();
+	}
+	
+	private class Background extends Task<Void> {
+		
+		@Override
+		protected Void call() {
+			new BashCommand().runCommand("arecord -d 2 -r 22050 -c 1 -i -t wav -f s16_LE foo.wav");
+			
+			return null;
+		}
+		
+		@Override
+		protected void done() {
+			Platform.runLater(() -> {
+				Game.getInstance().finishedRecording();
+			});
+		}
 	}
 }
