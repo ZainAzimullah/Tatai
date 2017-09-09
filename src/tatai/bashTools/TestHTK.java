906598c04e7a2c1a468b9942b05b5f6be6ed6cb7
Zain Azimullah (Linux) <zain.zain51@gmail.com>
2017-09-09 15:23:26 +1200

    Modified TestHTK to use setOnMedia rather than TasK

--- src/tatai/bashTools/TestHTK.java
+++ src/tatai/bashTools/TestHTK.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 
 import javafx.application.Application;
-import javafx.concurrent.Task;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
@@ -19,29 +18,12 @@ public class TestHTK extends Application {
 	@Override
 	public void start(Stage stage) {
 		BashCommand bash = new BashCommand();
-		bash.runCommand("arecord -d 2 -r 22050 -c 1 -i -t wav -f s16_LE foo.wav");		
+		bash.runCommand("arecord -d 2 -r 22050 -c 1 -i -t wav -f s16_LE foo.wav");
 		
-		new Thread(new Background()).start();
-	}
-	
-	private class Background extends Task<Void> {
-		@Override
-		protected Void call() {
-			Media media = new Media((new File(System.getProperty("user.dir") + "/foo.wav")).toURI().toString());
-			MediaPlayer player = new MediaPlayer(media);
-			player.play();
-			try {
-				Thread.sleep(2000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			return null;
-		}
-		
-		@Override
-		protected void succeeded() {
+		Media media = new Media((new File(System.getProperty("user.dir") + "/foo.wav")).toURI().toString());
+		MediaPlayer player = new MediaPlayer(media);
+		player.play();
+		player.setOnEndOfMedia(() -> {
 			VoiceRecogniser htk = new VoiceRecogniser();
 			
 			try {
@@ -51,8 +33,8 @@ public class TestHTK extends Application {
 			} catch (FileNotFoundException e) {
 				System.out.println("Could not find file");
 			}
-			
+
 			System.exit(0);
-		}
+		});
 	}
 }
