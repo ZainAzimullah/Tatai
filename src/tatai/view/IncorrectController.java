1be27532b88955aa6e22c73f7e34dd06f46cd0e7
Zain Azimullah (Linux) <zain.zain51@gmail.com>
2017-09-10 14:44:26 +1200

    Finished IncorrectController

--- src/tatai/view/IncorrectController.java
+++ src/tatai/view/IncorrectController.java
@@ -1,9 +1,28 @@
 package tatai.view;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import tatai.Game;
 
 public class IncorrectController extends SceneController {
 	
+	@FXML
+	private Label _number;
+	
+	@FXML
+	private Label _maoriWord;
+	
+	@FXML
+	private void initialize() {
+		_number.setText(Integer.toString(Game.getInstance().getNumber().getDigit()));
+		_maoriWord.setText(Game.getInstance().getNumber().toString());
+	}
+	
+	@FXML
+	private void tryAgain() {
+		Game.getInstance().rerecord();
+	}
+
 	@FXML
 	@Override
 	protected void returnToMainMenu() {
