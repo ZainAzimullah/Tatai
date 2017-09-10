3ecd7be9992136723b6d78aaded40aae754f653b
Zain Azimullah (Linux) <zain.zain51@gmail.com>
2017-09-10 12:59:49 +1200

    Added Label objects

--- src/tatai/view/CorrectController.java
+++ src/tatai/view/CorrectController.java
@@ -1,10 +1,21 @@
 package tatai.view;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 public class CorrectController extends SceneController {
 	
 	
+	@FXML
+	private Label _number;
+	
+	@FXML
+	private Label _maoriWord;
+	
+	@FXML
+	private void initialize() {
+		
+	}
 	
 	@FXML
 	@Override
