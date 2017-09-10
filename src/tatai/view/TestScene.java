d3614db87cd958908c1e0d5398b3b4dc7e0f9366
Zain Azimullah (Linux) <zain.zain51@gmail.com>
2017-09-10 14:45:45 +1200

    Renamed TestCorrect to TestScene

--- src/tatai/view/TestScene.java
+++ src/tatai/view/TestScene.java
@@ -8,7 +8,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class TestCorrect extends Application {
+public class TestScene extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws IOException {
