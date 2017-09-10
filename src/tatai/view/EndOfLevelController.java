c7ac7c2447a132c813e13332f6b603fe19fa69e8
Zain Azimullah (Linux) <zain.zain51@gmail.com>
2017-09-10 21:22:30 +1200

    Finished EndOfLevelController

--- src/tatai/view/EndOfLevelController.java
+++ src/tatai/view/EndOfLevelController.java
@@ -4,24 +4,55 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import tatai.Game;
+import tatai.util.Level;
+import tatai.util.Score;
+import tatai.util.Score.Result;
 
 public class EndOfLevelController extends SceneController {
+	
+	public static final int THRESHHOLD = 8;
+	
+	private boolean _mustRetry = false;
 	
 	@FXML
 	Button _next;
 	
 	@FXML
-	Label _numCorrect;
+	Label _numAchieved;
 	
 	@FXML
-	Label _numIncorrect;
+	Label _numFailed;
 	
 	@FXML
 	Label _totalNumOfAttempts;
 	
 	@FXML
 	private void initialize() {
+		Score score = Game.getInstance().getScore();
+		int numAchieved = score.getNumberOf(Result.CORRECT) + score.getNumberOf(Result.INCORRECT);
+		int numFailed = score.getNumberOf(Result.FAILED);
+		int totalNumOfAttempts = score.getTotalNumberOfAttempts();
 		
+		_numAchieved.setText(Integer.toString(numAchieved));
+		_numFailed.setText(Integer.toString(numFailed));
+		_totalNumOfAttempts.setText(Integer.toString(totalNumOfAttempts));
+		
+		if (numAchieved >= THRESHHOLD) {
+			_next.setText("Next Level");
+			_mustRetry = false;
+		} else {
+			_next.setText("Retry Level");
+			_mustRetry = true;
+		}
+	}
+	
+	@FXML
+	private void next() {
+		if (_mustRetry) {
+			Game.getInstance().createList(Level.HARD);
+		} else {
+			Game.getInstance().createList(Level.EASY);
+		}
 	}
 	
 	
