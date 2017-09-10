c20a5154b4f70dd78ac4ede45769f1dddd669363
Skippy <samuelzheng11@gmail.com>
2017-09-10 12:09:20 +1200

    cleared the correct controller class

--- src/tatai/view/CorrectController.java
+++ src/tatai/view/CorrectController.java
@@ -4,13 +4,5 @@ import javafx.fxml.FXML;
 
 public class CorrectController extends SceneController {
 
-	@FXML
-	private String _maoriNumber;
-	
-	public void setNumber(int maoriNumber) {
-		_maoriNumber = Integer.toString(maoriNumber);
-	}
-	
-	public void initalize
 	
 }
