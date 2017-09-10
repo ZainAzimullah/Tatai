16a9465a84bd2edc6e87ea949e1d7867652aee59
Zain Azimullah (Linux) <zain.zain51@gmail.com>
2017-09-10 12:56:27 +1200

    Made test class for Correct scene

--- src/tatai/view/CorrectController.java
+++ src/tatai/view/CorrectController.java
@@ -4,6 +4,8 @@ import javafx.fxml.FXML;
 
 public class CorrectController extends SceneController {
 	
+	
+	
 	@FXML
 	@Override
 	protected void returnToMainMenu() {
---
+++ src/tatai/view/TestCorrect.java
@@ -0,0 +1,26 @@
+package tatai.view;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class TestCorrect extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws IOException {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(this.getClass().getResource("Correct.fxml"));
+		Parent layout = loader.load();
+		Scene scene = new Scene(layout);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
