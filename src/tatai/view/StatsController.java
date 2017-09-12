08c4204ddacc81699c97fb6427732e698ad18c61
Zain Azimullah (Linux) <zain.zain51@gmail.com>
2017-09-12 22:28:46 +1200

    Finished StatsController

--- src/tatai/view/StatsController.java
+++ src/tatai/view/StatsController.java
@@ -1,12 +1,37 @@
 package tatai.view;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import tatai.util.FinalScore;
 
 public class StatsController extends SceneController {
 	
 	@FXML
+	private TableView<FinalScore> _table;
+	
+	@FXML
+	private TableColumn<FinalScore, String> _numCorrect;
+	
+	@FXML
+	private TableColumn<FinalScore, String> _numIncorrect;
+	
+	@FXML
+	private TableColumn<FinalScore, String> _numAttempts;
+	
+	@FXML
+	private TableColumn<FinalScore, String> _numMistakes;
+	
+	@FXML
+	private TableColumn<FinalScore, String> _level;
+	
+	@FXML
 	private void initialize() {
-		
+		_numCorrect.setCellValueFactory(data -> data.getValue().getNumberCorrect());
+		_numIncorrect.setCellValueFactory(data -> data.getValue().getNumberIncorrect());
+		_numAttempts.setCellValueFactory(data -> data.getValue().getNumberOfAttempts());
+		_numMistakes.setCellValueFactory(data -> data.getValue().getNumberOfMistakes());
+		_level.setCellValueFactory(data -> data.getValue().getLevel());
 	}
 	
 	@Override
