aa8db76a60521901c3ddc23cb7b184a972ba6af3
Zain Azimullah (Linux) <zain.zain51@gmail.com>
2017-09-15 15:59:57 +1200

    Added comments to MainMenuLoader

--- src/tatai/view/MainMenuLoader.java
+++ src/tatai/view/MainMenuLoader.java
@@ -5,6 +5,13 @@ import tatai.exceptions.TataiException;
 import tatai.view.controllers.SceneController;
 import tatai.view.controllers.mainMenuControllers.MainMenuController;
 
+/*
+ * This class is responsible for loading any scene belonging
+ * to the main menu.  This could be the main menu itself, or any
+ * scene that is accessible from the main menu except for 
+ * game scenes.  For loading game scenes, the ancestor class
+ * SceneLoader should be used.
+ */
 public class MainMenuLoader extends SceneLoader {
 	
 	public MainMenuLoader(Stage stage) {
@@ -13,6 +20,11 @@ public class MainMenuLoader extends SceneLoader {
 	
 	public MainMenuLoader() {}
 	
+	/*
+	 * This is a hook method that will be called from SceneLoader
+	 * that will be used to give the stage to the controller which will
+	 * be a MainMenuController in this case.
+	 */
 	@Override
 	protected void useController(SceneController controller) {
 		if (controller instanceof MainMenuController) {
