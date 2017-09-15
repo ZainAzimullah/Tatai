c0dbfd1937fb15d1c4d16ca94076a8f407382d86
Zain Azimullah (Linux) <zain.zain51@gmail.com>
2017-09-15 15:31:30 +1200

    Gave OutOfNumbersException default serial version id

--- src/tatai/exceptions/OutOfNumbersException.java
+++ src/tatai/exceptions/OutOfNumbersException.java
@@ -1,6 +1,9 @@
 package tatai.exceptions;
 
 public class OutOfNumbersException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
 	public OutOfNumbersException(String message) {
 		super(message);
 	}
