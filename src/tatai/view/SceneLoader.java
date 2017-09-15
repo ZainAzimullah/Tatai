a84d81e4e09541835d621f0dec5ec70cdeda4d27
Zain Azimullah (Linux) <zain.zain51@gmail.com>
2017-09-15 16:03:46 +1200

    Added comments to scene laoder. Enforced template method with final modifier

--- src/tatai/view/SceneLoader.java
+++ src/tatai/view/SceneLoader.java
@@ -9,6 +9,12 @@ import javafx.stage.Stage;
 import tatai.Game;
 import tatai.view.controllers.SceneController;
 
+/*
+ * This class is responsible for loading any game scene.
+ * It handles the FXML loading code automatically such that
+ * only the filename of the FXML needs to be given.
+ * The FXML file must be in the same package as this class.
+ */
 public class SceneLoader {
 	protected Stage _stage;
 	
@@ -16,28 +22,37 @@ public class SceneLoader {
 		_stage = stage;
 	}
 	
+	// Assume that the Game is being played if no stage
+	// was given.
 	public SceneLoader() {
 		_stage = Game.getInstance().getStage();
 	}
 	
-	// Load the scene
-	public void loadScene(String filename) {
+	// Load the scene.  This is a template method.
+	public final void loadScene(String filename) {
+		
+		// Build loader
 		FXMLLoader loader = new FXMLLoader();
 		loader.setLocation(this.getClass().getResource(filename));
 		
 		Parent layout = null;
 		
+		// Load scene
 		try {
 			layout = loader.load();
+			
+			// Pass to hook method
 			useController(loader.getController());
 		} catch (IOException e) {
 			e.printStackTrace();
 			System.exit(0);
 		}
 		
+		// Set scene to stage
 		Scene scene = new Scene(layout);
 		_stage.setScene(scene);
 	}
 	
+	// Optional hook method
 	protected void useController(SceneController controller) {}
 }
