cf687ca803c8d7febf9adc09887a0380b820dbaa
Zain Azimullah (Linux) <zain.zain51@gmail.com>
2017-10-10 21:54:43 +1300

    FInished singleton constructor

--- src/main/java/tatai/Game.java
+++ src/main/java/tatai/Game.java
@@ -1,4 +1,20 @@
 package tatai;
 
 public class Game {
+    private static Game _game;
+
+
+    private Game() {
+        // Singleton
+    }
+
+    public static Game getInstance() {
+        if (_game == null) {
+            _game = new Game();
+        }
+
+        return _game;
+    }
+
+    
 }
