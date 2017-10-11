baa948ef8d5c02613acb614896875095137ec4f8
Zain Azimullah (Linux) <zain.zain51@gmail.com>
2017-10-11 15:13:17 +1300

    Working on Game

--- src/main/java/tatai/Game.java
+++ src/main/java/tatai/Game.java
@@ -1,11 +1,25 @@
 package tatai;
 
+import javafx.stage.Stage;
+
 public class Game {
     private static Game _game;
+    private static Stage _stage;
 
+    private Game(Stage stage) {
+        _stage = stage;
+    }
 
     private Game() {
         // Singleton
+    }
+
+    public static Game getInitialInstance(Stage stage) {
+        if (_game == null) {
+            _game = new Game(stage);
+        }
+
+        return _game;
     }
 
     public static Game getInstance() {
@@ -16,5 +30,11 @@ public class Game {
         return _game;
     }
 
+    public void selectLevel() {
 
+    }
+
+    public Stage getStage() {
+        return _stage;
+    }
 }
