f1202cd501ca37bc799cf2bdab92a558912aec89
Zain Azimullah (Linux) <zain.zain51@gmail.com>
2017-10-11 20:45:27 +1300

    Created test loader

---
+++ src/main/java/tatai/view/TestLoader.java
@@ -0,0 +1,26 @@
+package tatai.view;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class TestLoader extends Application {
+
+    private static final String FILENAME = "Question.fxml";
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource(FILENAME));
+        Parent layout = loader.load();
+        Scene scene = new Scene(layout);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
