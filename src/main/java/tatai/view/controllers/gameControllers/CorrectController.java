7e67c361ee1d95751a546bcc6adf8b7f7b99f1a2
Zain Azimullah (Linux) <zain.zain51@gmail.com>
2017-10-12 14:35:56 +1300

    Initialization in CorrectController

--- src/main/java/tatai/view/controllers/gameControllers/CorrectController.java
+++ src/main/java/tatai/view/controllers/gameControllers/CorrectController.java
@@ -1,14 +1,27 @@
 package tatai.view.controllers.gameControllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import tatai.Game;
 import tatai.view.controllers.SceneController;
 
 public class CorrectController extends SceneController {
 
     @FXML
+    private Label _question;
+
+    @FXML
+    private Label _speech;
+
+    @FXML
     private void next() {
         Game.getInstance().newQuestion();
+    }
+
+    @FXML
+    private void initialize() {
+        _speech.setText(Game.getInstance().getSpeech());
+        _question.setText(Game.getInstance().getCurrentQuestion().toString());
     }
 
     @FXML
