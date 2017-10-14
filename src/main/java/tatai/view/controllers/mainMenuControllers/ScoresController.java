7e9aaa41229a7436a45d3f7a5a1bdcb42e94e94c
Zain Azimullah (Linux) <zain.zain51@gmail.com>
2017-10-14 22:53:06 +1300

    Made Scores controller

--- src/main/java/tatai/view/controllers/mainMenuControllers/ScoresController.java
+++ src/main/java/tatai/view/controllers/mainMenuControllers/ScoresController.java
@@ -1,34 +1,41 @@
 package tatai.view.controllers.mainMenuControllers;
 
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import tatai.practiceScore.PracticeFinalScore;
+import tatai.score.ScoreHistory;
+import tatai.score.ScoreProperties;
 import tatai.view.MainMenuLoader;
 
 public class ScoresController extends MainMenuController {
 	
 	@FXML
-	private TableView<PracticeFinalScore> _table;
+	private TableView<ScoreProperties> _table;
 	
 	@FXML
-	private TableColumn<PracticeFinalScore, String> _numCorrect;
+	private TableColumn<ScoreProperties, String> _date;
 	
 	@FXML
-	private TableColumn<PracticeFinalScore, String> _numIncorrect;
+	private TableColumn<ScoreProperties, String> _total;
+
+	@FXML
+	private TableColumn<ScoreProperties, String> _numMistakes;
 	
 	@FXML
-	private TableColumn<PracticeFinalScore, String> _numAttempts;
-	
-	@FXML
-	private TableColumn<PracticeFinalScore, String> _numMistakes;
-	
-	@FXML
-	private TableColumn<PracticeFinalScore, String> _level;
+	private TableColumn<ScoreProperties, String> _level;
 	
 	@FXML
 	private void initialize() {
+		_date.setCellValueFactory(data -> data.getValue().dateProperty());
+		_total.setCellValueFactory(data -> data.getValue().totalProperty());
+		_numMistakes.setCellValueFactory(data -> data.getValue().numMistakesProperty());
+		_level.setCellValueFactory(data -> data.getValue().difficultyProperty());
 
+		ScoreHistory history = new ScoreHistory();
+		ObservableList<ScoreProperties> data = history.getObservableList();
+		_table.setItems(data);
 	}
 	
 	@FXML
