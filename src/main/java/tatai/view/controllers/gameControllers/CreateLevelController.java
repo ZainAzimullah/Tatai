f2c8fb3bc2a8aceb9092647d0053a6e747b4284d
Zain Azimullah (Linux) <zain.zain51@gmail.com>
2017-10-18 20:49:29 +1300

    Added listener to _max textfield

--- src/main/java/tatai/view/controllers/gameControllers/CreateLevelController.java
+++ src/main/java/tatai/view/controllers/gameControllers/CreateLevelController.java
@@ -2,7 +2,9 @@ package tatai.view.controllers.gameControllers;
 
 import com.jfoenix.controls.JFXCheckBox;
 import com.jfoenix.controls.JFXTextField;
+import javafx.beans.property.StringProperty;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.SpinnerValueFactory;
 import tatai.Game;
@@ -10,6 +12,8 @@ import tatai.expressionModel.custom.CustomLevelSettings;
 import tatai.view.controllers.SceneController;
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CreateLevelController extends SceneController{
 
@@ -18,6 +22,9 @@ public class CreateLevelController extends SceneController{
 
     @FXML
     private JFXTextField _name, _max;
+
+    @FXML
+    private Label _valid;
 
     @FXML
     private void save() {
@@ -42,7 +49,20 @@ public class CreateLevelController extends SceneController{
 
     @FXML
     private void initialize() {
+        _valid.setVisible(false);
 
+        _max.textProperty().addListener((observable, oldValue, newValue) -> {
+            String input = newValue.toString();
+            Pattern pattern = Pattern.compile("\\d+");
+            Matcher matcher = pattern.matcher(newValue.toString());
+
+            if (matcher.matches()) {
+                _valid.setVisible(false);
+            } else {
+                _valid.setVisible(true);
+                _valid.setText("You must enter a number between 1 and 99");
+            }
+        });
     }
     // Cancel button
     @FXML
