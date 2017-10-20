bb19b84038e3a1808f8d0ff4a6f6a3fd80dc5050
Zain Azimullah (Linux) <zain.zain51@gmail.com>
2017-10-20 16:25:28 +1300

    radio button logic complete

--- src/main/java/tatai/view/controllers/mainMenuControllers/StatsSceneController.java
+++ src/main/java/tatai/view/controllers/mainMenuControllers/StatsSceneController.java
@@ -1,8 +1,10 @@
 package tatai.view.controllers.mainMenuControllers;
 
+import com.jfoenix.controls.JFXRadioButton;
 import javafx.fxml.FXML;
 import javafx.scene.chart.*;
 import javafx.scene.control.Label;
+import javafx.scene.control.ToggleGroup;
 import tatai.score.Score;
 import tatai.score.ScoreHistory;
 import tatai.score.ScoreProperties;
@@ -20,8 +22,12 @@ public class StatsSceneController extends MainMenuController {
     private Label _average, _best;
 
     @FXML
-    private void initialize() {
+    private JFXRadioButton _all, _easy, _medium, _hard, _custom;
 
+
+
+    @FXML
+    private void initialize() {
         ScoreHistory history = new ScoreHistory();
 
         _average.setText(Double.toString(history.getMean()));
@@ -35,16 +41,45 @@ public class StatsSceneController extends MainMenuController {
         _chart.getYAxis().setAutoRanging(false);
         _chart.getYAxis().setLabel("Score");
 
-        XYChart.Series series = new XYChart.Series();
-        populateSeries(series, history.getScores());
+        ToggleGroup group = new ToggleGroup();
+        _easy.setToggleGroup(group);
+        _medium.setToggleGroup(group);
+        _hard.setToggleGroup(group);
+        _custom.setToggleGroup(group);
+        _all.setToggleGroup(group);
 
-        _chart.getData().add(series);
+        _all.selectedProperty().addListener((observable, oldValue, newValue) -> {
+            generateChart(history.getScores());
+        });
+
+        _easy.selectedProperty().addListener((observable, oldValue, newValue) -> {
+            generateChart(history.getScores(Difficulty.EASY));
+        });
+
+        _medium.selectedProperty().addListener((observable, oldValue, newValue) -> {
+            generateChart(history.getScores(Difficulty.MEDIUM));
+        });
+
+        _hard.selectedProperty().addListener((observable, oldValue, newValue) -> {
+            generateChart(history.getScores(Difficulty.HARD));
+        });
+
+        _custom.selectedProperty().addListener((observable, oldValue, newValue) -> {
+            generateChart(history.getScores(Difficulty.CUSTOM));
+        });
+
+        _all.selectedProperty().setValue(true);
     }
 
-    private void populateSeries(XYChart.Series series, ArrayList<Score> scores) {
+    private void generateChart(ArrayList<Score> scores) {
+        _chart.getData().clear();
+
+        XYChart.Series series = new XYChart.Series();
         for (Score score: scores) {
             series.getData().add(new XYChart.Data(score.getTime(), score.getTotal()));
         }
+
+        _chart.getData().add(series);
     }
 
     @FXML
